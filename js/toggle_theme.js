8092001184adf99ed43ffecce29a73de75b4dda6
piercebhunt <huntpie@gmail.com>
2024-02-26 21:08:05 -0500

    new changes

--- js/toggle_theme.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const toggleSwitch = document.getElementById('theme-toggle');
-  toggleSwitch.addEventListener('change', function() {
-      const theme = this.checked ? 'dark' : 'light';
-      document.body.setAttribute('data-theme', theme);
-  });
-});
-
